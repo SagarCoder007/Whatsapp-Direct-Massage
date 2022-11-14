f5640b7ebb17cda6b469bcbb4aa2544dd9e086a2
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-11-14 15:47:12 +0530

    Adding features

--- main.js
+++ main.js
@@ -4,12 +4,34 @@ const button = document.getElementById("btn")
 
 
 button.addEventListener("click", () => {
-
   const wa = "https://Wa.me/"
-  const output = wa + "+" + country.value + phone.value
 
-  document.getElementById("link").href = output
-  country.value = null
-  phone.value = null
+  if (country.value == "") {
+    country.style.border = "2px solid red"
+
+    event.preventDefault()
+  }
+
+  else if (phone.value == "") {
+    event.preventDefault()
+    phone.style.border = "2px solid red"
+  }
+
+  else {
+    event.preventDefault()
+    const output = wa + "+" + country.value + phone.value
+    country.style.border = "1px solid black"
+    phone.style.border = "1px solid black"
+    document.getElementById("link").href = output
+    country.value = null
+    phone.value = null
+
+    let data = []
+    data.push(output)
+    console.log(data)
+
+  }
+
+
 
 })
