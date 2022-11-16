c3eaecffc8478cbf22f942634b577d37a47ddbe2
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-11-16 13:10:43 +0530

    Improve js code

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const country = document.getElementById("country")
 const phone = document.getElementById("phone")
 const button = document.getElementById("btn")
 const url = "https://Wa.me/"
+const link = document.getElementById("link")
 
 button.addEventListener("click", () => {
-  event.preventDefault()
-
   phoneVal = phone.value
   countryVal = country.value
 
@@ -16,18 +15,13 @@ button.addEventListener("click", () => {
 
     if (get == null) {
       data = []
-
     }
 
     else {
       data = JSON.parse(get)
-
-
     }
     data.push("+" + countryVal + phoneVal)
     let set = localStorage.setItem(1, JSON.stringify(data))
-
-
   }
 
   else {
@@ -37,53 +31,34 @@ button.addEventListener("click", () => {
 
 
   sshow()
+  link.href = url + "+" + country.value + phone.value
   country.value = null
   phone.value = null
-
-
-
-
-
-
-
-
 })
 
 
-
-
-
-
-
-
+// Show Funsion.
 let sshow = (() => {
-
   let get = localStorage.getItem(JSON.parse(1))
 
   if (get == null) {
     data = []
   }
-
   else {
     data = JSON.parse(get)
   }
 
   let insert = ""
-
   data.forEach((item, index) => {
     let table = document.getElementById("table")
-
     insert += `<tr>
             <td>${index+1}</td>
             <td id="text">${item}</td>
             <td><a href="${url}${item}"class"goto"><i class="fa-brands fa-whatsapp"></i></a></td>
             <td onClick="del(${index})"><i class="fa-solid fa-trash"></i></td>
           </tr>`
-
     table.innerHTML = insert
-
   })
-
 })
 
 sshow()
@@ -91,15 +66,12 @@ sshow()
 
 
 
+
+// Delete Button Funsion.
 let del = (index) => {
   let get = localStorage.getItem(JSON.parse(1))
   data = JSON.parse(get)
-
   data.splice(index, 1)
-
   localStorage.setItem(1, JSON.stringify(data))
-
   location.reload()
-
-
 }
